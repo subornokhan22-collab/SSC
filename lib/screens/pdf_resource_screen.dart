f7e65d508ae92888ab6ffbc857404c0d44d401d9
subornokhan22-collab <subornokhan22@gmail.com>
2026-08-01 23:48:47 +0600

    Update pdf_resource_screen.dart

--- lib/screens/pdf_resource_screen.dart
+++ lib/screens/pdf_resource_screen.dart
@@ -1,93 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class PDFResourceScreen extends StatefulWidget {
-  const PDFResourceScreen({Key? key}) : super(key: key);
+class PdfResourceScreen extends StatefulWidget {
+  const PdfResourceScreen({super.key});
 
   @override
-  State<PDFResourceScreen> createState() => _PDFResourceScreenState();
+  State<PdfResourceScreen> createState() => _PdfResourceScreenState();
 }
 
-class _PDFResourceScreenState extends State<PDFResourceScreen> {
+class _PdfResourceScreenState extends State<PdfResourceScreen> {
   late final WebViewController _controller;
-  bool _isLoading = true;
-  bool _hasError = false;
+  bool _loading = true;
 
-  final String _driveUrl = 'https://drive.google.com/embeddedfolderview?id=19UW5mGKcBgorLSmO-joBer0HBodGA65G#grid';
+  // TODO: replace with your actual Google Drive folder share link
+  static const String driveUrl = 'https://drive.google.com/drive/folders/YOUR_FOLDER_ID';
 
   @override
   void initState() {
     super.initState();
-    _initWebView();
-  }
-
-  void _initWebView() {
     _controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..setBackgroundColor(Colors.white)
-      ..setNavigationDelegate(
-        NavigationDelegate(
-          onPageStarted: (String url) {
-            setState(() {
-              _isLoading = true;
-              _hasError = false;
-            });
-          },
-          onPageFinished: (String url) {
-            setState(() {
-              _isLoading = false;
-            });
-          },
-          onWebResourceError: (WebResourceError error) {
-            setState(() {
-              _isLoading = false;
-              _hasError = true;
-            });
-          },
-        ),
-      )
-      ..loadRequest(Uri.parse(_driveUrl));
+      ..setNavigationDelegate(NavigationDelegate(
+        onPageStarted: (_) => setState(() => _loading = true),
+        onPageFinished: (_) => setState(() => _loading = false),
+      ))
+      ..loadRequest(Uri.parse(driveUrl));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('PDF রিসোর্স', style: TextStyle(fontWeight: FontWeight.bold)),
-        backgroundColor: const Color(0xFF1A82BB),
-        foregroundColor: Colors.white,
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.refresh),
-            onPressed: () {
-              _controller.reload();
-            },
-          ),
-        ],
+        title: const Text('রিসোর্স / PDF'),
+        actions: [IconButton(icon: const Icon(Icons.refresh), onPressed: () => _controller.reload())],
       ),
       body: Stack(
         children: [
-          if (!_hasError) WebViewWidget(controller: _controller),
-          if (_isLoading)
-            const Center(
-              child: CircularProgressIndicator(color: Color(0xFF1A82BB)),
-            ),
-          if (_hasError)
-            Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Icon(Icons.wifi_off, size: 48, color: Colors.grey),
-                  const SizedBox(height: 12),
-                  const Text('ইন্টারনেট সংযোগ সংযোগ পরীক্ষা করে পুনরায় চেষ্টা করুন।'),
-                  const SizedBox(height: 12),
-                  ElevatedButton(
-                    onPressed: () => _controller.reload(),
-                    child: const Text('পুনরায় চেষ্টা করুন'),
-                  ),
-                ],
-              ),
-            ),
+          WebViewWidget(controller: _controller),
+          if (_loading) const LinearProgressIndicator(),
         ],
       ),
     );
